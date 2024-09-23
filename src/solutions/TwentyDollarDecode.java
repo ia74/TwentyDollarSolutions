b5968d1d892f9a8e4a1bed7e428da87854cdb6b1
ia74 <68617740+ia74@users.noreply.github.com>
2024-09-23 09:14:03 -0500

    - Decode - Encrypted Words complete

--- src/solutions/TwentyDollarDecode.java
+++ src/solutions/TwentyDollarDecode.java
@@ -14,32 +14,12 @@ public class TwentyDollarDecode {
             Queue<String> stack = new LinkedList<>();
             String line = scan.nextLine();
             int curc = 0;
-            validate(line, curc, stack);
+
             System.out.println(stack);
             while(!stack.isEmpty()) {
                 System.out.print(stack.remove());
             }
             System.out.println();
-        }
-    }
-    static void validate(String line, int curc, Queue<String> stack) {
-        for(String l : line.split("")) {
-            if(l.equals("[")) {
-                int repeat = Integer.parseInt(String.valueOf(line.charAt(curc - 1)));
-                String toRepeat = line.substring(curc + 1).split("]")[0];
-                for(String c : toRepeat.split("")) {
-                    if(c.equals("[")) {
-                        toRepeat = toRepeat.split("\\[")[0];
-                        toRepeat = toRepeat.substring(0, toRepeat.length()-1);
-                    }
-                }
-                System.out.println(toRepeat);
-//                a = toRepeat;
-                for(int i = 0; i < repeat; i++) {
-                    stack.add(toRepeat);
-                }
-            }
-            curc++;
         }
     }
 }
---
+++ src/solutions/TwentyDollarEncryptedWords.java
@@ -0,0 +1,36 @@
+package solutions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TwentyDollarEncryptedWords {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(new File("pr74.dat"));
+        int sets = scan.nextInt();
+        scan.nextLine();
+        while(sets-- > 0) {
+            String line = scan.nextLine();
+            String[] spaces = line.split(" ");
+            StringBuilder rebuilt = new StringBuilder();
+            for(int i = 1; i < spaces.length + 1; i++) {
+                if(i % 2 == 0) {
+                    rebuilt.append(transform(spaces[i - 1], i) + " ");
+                } else {
+                    rebuilt.append(new StringBuilder(spaces[i - 1]).reverse().toString() + " ");
+                }
+            }
+            System.out.println(rebuilt);
+        }
+    }
+    static String transform(String inp, int cycles) {
+        if(cycles > inp.length()) return inp;
+        cycles %= inp.length();
+        String transformed = "";
+        // last n cycles go to end
+        // SCIENCE 2 = CESCIEN
+        transformed += inp.substring(inp.length() - cycles);
+        transformed += inp.substring(0, inp.length() - cycles);
+        return transformed;
+    }
+}

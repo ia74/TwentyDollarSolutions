c089418397c16b1954314d782eb57fce83f6e55a
ia74 <68617740+ia74@users.noreply.github.com>
2024-10-30 09:33:12 -0500

    fix

--- src/solutions/TwentyDollarRamiro.java
+++ src/solutions/TwentyDollarRamiro.java
@@ -11,17 +11,24 @@ public class TwentyDollarRamiro {
         while(n-->0) {
             String line = scan.nextLine();
             String[] spc = line.split(" ");
-            int numer = Integer.parseInt(spc[0], 16);
-            int numerdos = Integer.parseInt(spc[1], 16);
-            String numers = Integer.toBinaryString(numer);
-            String numerdoss = Integer.toBinaryString(numerdos);
+            long numer = Long.parseLong(spc[0], 16);
+            long numerdos = Long.parseLong(spc[1], 16);
+            String numers = Long.toBinaryString(numer);
+            String numerdoss = Long.toBinaryString(numerdos);
             int distance = 0;
+            numers = pad(numers);
+            numerdoss = pad(numerdoss);
             for(int i = 0; i < numers.length(); i++) {
-                String a = numers.substring(i, i+1);
-                String b = numerdoss.substring(i, i+1);
-                if(a.equals(b)) distance++;
+                String a = String.valueOf(numers.charAt(i));
+                String b = String.valueOf(numerdoss.charAt(i));
+                if(!a.equals(b)) distance++;
             }
             System.out.println(distance);
         }
+    }
+    public static String pad(String a) {
+        String b = a;
+        if(b.length() < 32) b = "0".repeat(32 - a.length()) + b;
+        return b;
     }
 }

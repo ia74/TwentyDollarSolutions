06cdd99d9fc8f029c5c18f702c1a4906e51801a2
aiden <aidenjs9@icloud.com>
2024-12-10 09:37:01 -0600

    Kendrick lamar not like us

--- src/solutions/TwentyDollarPr153MealPlannerSortableLebronJamesLetterCounterArrayListMachineSingletonCollectorConcreteClassListStackHandlerManagerCreatorBuilderHelperClassTypeGenericInferDynamic.java
+++ src/solutions/TwentyDollarPr153MealPlannerSortableLebronJamesLetterCounterArrayListMachineSingletonCollectorConcreteClassListStackHandlerManagerCreatorBuilderHelperClassTypeGenericInferDynamic.java
@@ -2,21 +2,36 @@ package solutions;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TwentyDollarPr153MealPlannerSortableLebronJamesLetterCounterArrayListMachineSingletonCollectorConcreteClassListStackHandlerManagerCreatorBuilderHelperClassTypeGenericInferDynamic {
-    public static class Food {
-        String food;
-        int fat;
-        int protein;
-        int carbs;
-        public Food(String f, int fat, int protein, int carbs) {
-            this.food = f;
-            this.fat = fat;
-            this.protein = protein;
-            this.carbs = carbs;
+    public static int does(Integer[] array, int sum, int min) {
+        int len = array.length;
+        int[][] sumTo = new int[len+1][sum+1];
+
+        for(int i = 0; i <= len; i ++) {
+            sumTo[i][0] = i;
         }
+
+        int count = 0;
+        for(int i = 1; i <= len; i ++) {
+            for(int j = 1; j <= sum; j ++) {
+                if(sumTo[i-1][j] > sumTo[i][j-1]) {
+                    sumTo[i][j] = sumTo[i-1][j];
+
+                } else {
+                    sumTo[i][j] = sumTo[i-1][j];
+                }
+            }
+        }
+
+
+        System.out.println(Arrays.deepToString(sumTo));
+        System.out.println(count);
+        return sumTo[len][sum];
     }
     public static class Range {
         int min;
@@ -39,42 +54,21 @@ public class TwentyDollarPr153MealPlannerSortableLebronJamesLetterCounterArrayLi
             Range fat = Range.fromStr(ranges[0]);
             Range protein = Range.fromStr(ranges[1]);
             Range carbs = Range.fromStr(ranges[2]);
+            ArrayList<Integer> fats = new ArrayList<>();
+            ArrayList<Integer> proteins = new ArrayList<>();
+            ArrayList<Integer> carbss = new ArrayList<>();
 
             int nextFoods = scan.nextInt();
             scan.nextLine();
-            ArrayList<Food> yum = new ArrayList<>();
             for(int i = 0; i < nextFoods; i++) {
                 String[] ln = scan.nextLine().split(" ");
-                yum.add(new Food(ln[0], Integer.parseInt(ln[1]), Integer.parseInt(ln[2]), Integer.parseInt(ln[3])));
+                fats.add(Integer.parseInt(ln[1]));
+                proteins.add(Integer.parseInt(ln[2]));
+                carbss.add(Integer.parseInt(ln[3]));
             }
-
-            int count = 0;
-            int count2 = 0;
-            int count3 = 0;
-            for(Food food : yum) {
-                int curr = fat.max;
-                while(curr > fat.min) {
-                    if(food.fat == 0) break;
-                    curr -= food.fat;
-                    count++;
-                    System.out.println(count +" - " + curr + " - " + food.fat);
-                }
-                curr = protein.max;
-                while(curr > protein.min) {
-                    if(food.protein == 0) break;
-                    curr -= food.protein;
-                    count2++;
-                }
-                curr = carbs.max;
-                while(curr > carbs.min) {
-                    if(food.carbs == 0) break;
-                    curr -= food.carbs;
-                    count3++;
-                }
-            }
-            System.out.println(count);
-            System.out.println(count2);
-            System.out.println(count3);
+            System.out.println(does(fats.toArray(new Integer[0]), fat.max, fat.min));
+            System.out.println(does(proteins.toArray(new Integer[0]), protein.max, protein.min));
+            System.out.println(does(carbss.toArray(new Integer[0]), carbs.max, carbs.min));
         }
     }
 }

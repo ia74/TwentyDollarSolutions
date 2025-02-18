8f92cbdaf5e0107172481c4c3faa68658258a76b
aiden <aidenjs9@icloud.com>
2025-02-18 09:20:28 -0600

    asafafasdfewqwewffewef

---
+++ src/solutions/TwentyDollarRadialSwap.java
@@ -0,0 +1,18 @@
+package solutions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TwentyDollarRadialSwap {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("swap.dat"));
+        int noSets = scan.nextInt();
+        scan.nextLine();
+        while(noSets-- > 0) {
+            String ln = scan.nextLine();
+
+        }
+    }
+}

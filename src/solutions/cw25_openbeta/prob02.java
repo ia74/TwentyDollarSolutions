28d6fa64668365c205f7ec83a9f3bc8f38d6068b
ia74 <68617740+ia74@users.noreply.github.com>
2025-03-04 10:33:57 -0600

    add real source from comp

---
+++ src/solutions/cw25_openbeta/prob02.java
@@ -0,0 +1,11 @@
+package solutions.cw25_openbeta;
+import java.util.*;
+import java.io.*;
+public class prob02 {
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(new File("input.txt"));
+		while(scan.hasNextInt()) {
+			
+		}
+	}
+}
---
+++ src/solutions/cw25_openbeta/prob03.java
@@ -0,0 +1,60 @@
+package solutions.cw25_openbeta;
+import java.util.*;
+import java.io.*;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+
+public class prob03 {
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(new File("judging/cw2025_openbeta/prob03-1-in.txt"));
+		while(scan.hasNextLine()) {
+			String ln = scan.nextLine();
+			if(ln.equals("END")) break;
+			
+			Scanner leb = new Scanner(ln);
+			double n1 = leb.nextInt();
+			double n2 = leb.nextInt();
+			String operand = leb.next();
+			double whatThey = leb.nextDouble();
+			double trueout = 0;
+			
+			switch(operand) {
+				case "DIVIDE":
+					operand = "/";
+					trueout = n1 / n2;
+					break;
+				case "MULTIPLY":
+					operand = "*";
+					trueout = n1 * n2;
+					break;
+				case "SUBTRACT":
+					operand = "-";
+					trueout = n1 - n2;
+					break;
+				case "ADD":
+					operand = "+";
+					trueout = n1 + n2;
+					break;
+			}
+			
+			if(whatThey == trueout) {
+				System.out.println(
+						change(trueout) + " is correct for " + change(n1) + " " + operand +" " + change(n2)		
+				);
+			} else {
+				System.out.println(
+						change(n1) + " " + operand +" " + change(n2) + " = " + change(trueout) +", not " + change(whatThey)
+				);
+			}
+		}
+	}
+	
+	static String change(int value) {return change((double) value);} 
+	static String change(double value) 
+    { 
+		DecimalFormat df = new DecimalFormat("#0.0");
+		df.setRoundingMode(RoundingMode.DOWN);
+        return df.format(value);
+      } 
+}
